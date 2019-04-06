ccc8b67c428f592140a6214e8fc8aa336589c8aa
Stella Mainar <mainars09@alumni.hanover.edu>
2019-04-05 20:12:38 -0600

    Add attributes to Game
    Co-Authored-By: Alexandra Chakeres <chakeresa@users.noreply.github.com>
    Co-Authored-By: Kyle Cornelissen <kylecornelissen@users.noreply.github.com>

--- lib/game.rb
+++ lib/game.rb
@@ -5,7 +5,15 @@ class Game
               :team_ids,
               :goals,
               :home_win,
-              :settled_in
+              :settled_in,
+              :shots,
+              :hits,
+              :pim,
+              :power_play_opportunities,
+              :power_play_goals,
+              :face_off_win_percentage,
+              :giveaways,
+              :takeaways
 
   def initialize(game_info, relevant_game_team_stats)
     @game_id = game_info[:game_id]
@@ -16,6 +24,14 @@ class Game
     @home_win = game_info[:outcome].split[0] == "home"
     @settled_in = game_info[:outcome].split[2]
     @coaches = {home: relevant_game_team_stats[:home][:head_coach], away: relevant_game_team_stats[:away][:head_coach]}
+    @shots = {home: relevant_game_team_stats[:home][:shots], away: relevant_game_team_stats[:away][:shots]}
+    @hits = {home: relevant_game_team_stats[:home][:hits], away: relevant_game_team_stats[:away][:hits]}
+    @pim = {home: relevant_game_team_stats[:home][:pim], away: relevant_game_team_stats[:away][:pim]}
+    @power_play_opportunities = {home: relevant_game_team_stats[:home][:powerplayopportunities], away: relevant_game_team_stats[:away][:powerplayopportunities]}
+    @power_play_goals = {home: relevant_game_team_stats[:home][:powerplaygoals], away: relevant_game_team_stats[:away][:powerplaygoals]}
+    @face_off_win_percentage = {home: relevant_game_team_stats[:home][:faceoffwinpercentages], away: relevant_game_team_stats[:away][:faceoffwinpercentages]}
+    @giveaways = {home: relevant_game_team_stats[:home][:giveaways], away: relevant_game_team_stats[:away][:giveaways]}
+    @takeaways = {home: relevant_game_team_stats[:home][:takeaways], away: relevant_game_team_stats[:away][:takeaways]}
   end
 
   def total_goals
