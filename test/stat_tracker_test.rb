521e21f09e4dfd85c1c29fca4d6d0d86c79094d3
Stella Mainar <mainars09@alumni.hanover.edu>
2019-04-09 16:05:31 -0600

    Add a space between the remaining tests for Iteration 5 in StatTracker
    Co-Authored-By: Alexandra Chakeres <chakeresa@users.noreply.github.com>
    Co-Authored-By: Kyle Cornelissen <kylecornelissen@users.noreply.github.com>

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -304,10 +304,17 @@ class StatTrackerTest < Minitest::Test
   end
 
 # winningest_coach	Name of the Coach with the best win percentage for the season	String
+
 # worst_coach	Name of the Coach with the worst win percentage for the season	String
+
 # most_accurate_team	Name of the Team with the best ratio of shots to goals for the season	String
+
 # least_accurate_team	Name of the Team with the worst ratio of shots to goals for the season	String
+
 # most_hits	Name of the Team with the most hits in the season	String
+
 # fewest_hits	Name of the Team with the fewest hits in the season	String
+
 # power_play_goal_percentage	Percentage of goals that were power play goals for the season (rounded to the nearest 100th)	Float
+
 end
