130cc14aa447b341583717d80ca922df54cb3b78
Stella Mainar <mainars09@alumni.hanover.edu>
2019-04-09 15:59:09 -0600

    Add biggest_bust method and all_teams_in_post_season helper to SeasonStats
    Co-Authored-By: Alexandra Chakeres <chakeresa@users.noreply.github.com>
    Co-Authored-By: Kyle Cornelissen <kylecornelissen@users.noreply.github.com>

--- lib/season_stats.rb
+++ lib/season_stats.rb
@@ -1,6 +1,20 @@
 module SeasonStats
-  def biggest_bust
-    # Name of the team with the biggest decrease between regular season and postseason win percentage.
+  def all_teams_in_post_season(season_id)
+    @teams.values.find_all do |team_object|
+      if team_object.seasons_hash.keys.include?(season_id)
+        season_object = team_object.seasons_hash[season_id]
+        season_object.total_post_game_count != 0
+      else
+        false
+      end
+    end
+  end
+
+  def biggest_bust(season_id)
+    all_teams_in_post_season(season_id).max_by do |team_object|
+      season_object = team_object.seasons_hash[season_id]
+      season_object.reg_seas_win_percentage - season_object.post_seas_win_percentage
+    end.team_name
   end
 
 end
